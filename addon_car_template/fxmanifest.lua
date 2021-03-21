dad3368e7fb173dcc4deb74ffff7780b2adbc963
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-03-20 23:21:47 -0400

    Update fxmanifest.lua

--- addon_car_template/fxmanifest.lua
+++ addon_car_template/fxmanifest.lua
@@ -13,8 +13,8 @@ data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
-data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
-data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+-- data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+-- data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
 
 client_script {
   'vehicle_names.lua'
