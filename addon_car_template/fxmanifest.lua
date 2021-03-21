7969ca1a9115c5cf20479c69e6442f9a59ca55b2
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-03-20 22:41:22 -0400

    Update and rename __resource.lua to fxmanifest.lua

---
+++ addon_car_template/fxmanifest.lua
@@ -0,0 +1,22 @@
+  
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+description 'Globbing Template made by PLOK'
+
+files {
+    'data/**/*.meta'
+}
+ 
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+
+client_script {
+  'vehicle_names.lua'
+}
