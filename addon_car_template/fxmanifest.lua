3d0feb2f141b16a07bd2538ea0288d0da57f3503
Tuan <79166662+tuanpl0x@users.noreply.github.com>
2022-03-09 10:10:05 +0100

    Update fxmanifest.lua
    Made simplier and more effective.

--- addon_car_template/fxmanifest.lua
+++ addon_car_template/fxmanifest.lua
@@ -1,19 +1,16 @@
 fx_version 'cerulean'
+game 'gta5'
 
-game { 'gta5' }
-
-description 'Globbing Template made by PLOK'
+description 'Globbing Template made by PLOK, reedited by Tuan'
 
 files {
-    'data/**/*.meta'
+	'data/**/*.meta'
 }
  
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
--- data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
--- data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+data_file 'HANDLING_FILE'			'data/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'	'data/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/**/*.meta'
+data_file 'CARCOLS_FILE'			'data/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/**/*.meta'
 
 client_script 'vehicle_names.lua'
