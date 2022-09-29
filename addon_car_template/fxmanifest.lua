69db3e4ff7494b41b4810d3de4f71a828cfc4bf6
gjoop <joop_guillaume@live.fr>
2022-09-30 00:13:38 +1100

    audio update

--- addon_car_template/fxmanifest.lua
+++ addon_car_template/fxmanifest.lua
@@ -5,7 +5,10 @@ game { 'gta5' }
 description 'Globbing Template made by PLOK'
 
 files {
-    'data/**/*.meta'
+    'data/**/*.meta',
+	'audioconfig/*.dat151.rel',
+	'audioconfig/*.dat54.rel',
+	'sfx/**/*.awc'
 }
  
 data_file 'HANDLING_FILE' 'data/**/handling.meta'
@@ -13,7 +16,18 @@ data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
--- data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
--- data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+
+--AUDIO
+--vehicle1
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vehicle1_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vehicle1_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vehicle1'
+--vehicle2
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vehicle2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vehicle2_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vehicle2'
+
 
 client_script 'vehicle_names.lua'
---
+++ addon_car_template/sfx/dlc_vehicle2/readme.md
@@ -0,0 +1,1 @@
+## Add all your your '*.awc' files for vehicle 2 in this folder
